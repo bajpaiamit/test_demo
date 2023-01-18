2289decd1ac5ecb7d65b80cdb3abecb47174bd4a
bajpaiamit <amitbajpai@rediffmail.com>
2023-01-18 19:47:58 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "gcp_credentials" {
+    type = string
+    description = "Location of file"
+}
+variable "gcp_project_id" {
+    type = string
+    description = "GCP Project ID"
+}
+variable "gcp_region" {
+    type = string
+    description = "GCP Region"
+}
